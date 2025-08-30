b0e1eba6cb7fc942b7251315034c16ecf44ec4b4
Hanz4your <155755456+Hanz4your@users.noreply.github.com>
2025-08-31 00:32:17 +0700

    Add files via upload

---
+++ laatihan/bentuk.js
@@ -0,0 +1,15 @@
+function Menujuigdimas() {
+    alert("Menuju instagram Dimas");
+}
+
+function Menujuigharis() {
+    alert("Menuju instagram haris");
+}
+
+function Menujuigtoro() {
+    alert("Menuju instagram toro");
+}
+
+function qrbayar() {
+    alert("Hati-Hati Penipuan!!!! QR yang asli hanya atas nama HRD Community");
+}
