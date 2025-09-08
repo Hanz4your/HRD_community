b8a849008033b7ca594a81a1021903f4b05929f5
ToroWeb <rayhansyofiyantoro@gmail.com>
2025-09-08 21:52:02 +0700

    initial commit of my project

---
+++ bentuk.js
@@ -0,0 +1,25 @@
+function Menujuigdimas() {
+    alert("Menuju instagram Dimas");
+}
+
+function Menujuigharis() {
+    alert("Menuju instagram haris");
+}
+
+function Menujuigtoro() {
+    alert("Menuju instagram toro");
+}
+
+function qrbayar() {
+    alert("Hati-Hati Penipuan!!!! QR yang asli hanya atas nama HRD Community");
+}
+
+function showText(elementId) {
+        const textElement = document.getElementById(elementId);
+        textElement.classList.add('visible');
+    }
+
+function hideText(elementId) {
+        const textElement = document.getElementById(elementId);
+        textElement.classList.remove('visible');
+    }
