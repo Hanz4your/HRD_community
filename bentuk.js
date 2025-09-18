6fc5f19ee6db8f79e9466240c6b76a40256f09f1
ToroWeb <rayhansyofiyantoro@gmail.com>
2025-09-19 01:35:27 +0700

    memperbarui code

--- bentuk.js
+++ bentuk.js
@@ -15,11 +15,18 @@ function qrbayar() {
 }
 
 function showText(elementId) {
-        const textElement = document.getElementById(elementId);
-        textElement.classList.add('visible');
-    }
+    const textElement = document.getElementById(elementId);
+    textElement.classList.add('visible');
+}
 
 function hideText(elementId) {
-        const textElement = document.getElementById(elementId);
-        textElement.classList.remove('visible');
-    }
+    const textElement = document.getElementById(elementId);
+    textElement.classList.remove('visible');
+}
+
+const resetButton = document.getElementById('button');
+const iframe = document.querySelector('iframe[name="nama"]');
+const initialSrc = 'editingfoto/bg-iframe.html';
+resetButton.addEventListener('click', () => {
+    iframe.src = initialSrc;
+});
